a8fa53739fe2fe5d761cad23acb12d29c3337ee0
mauricio-alves <mauricioalvesnutri@gmail.com>
2024-11-21 18:05:52 -0300

    signup page done

--- src/app/views/signup/signup.component.ts
+++ src/app/views/signup/signup.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SignupformComponent } from '../../components/signupform/signupform.component';
 import { ApplogoComponent } from '../../components/applogo/applogo.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [SignupformComponent, ApplogoComponent],
+  imports: [SignupformComponent, ApplogoComponent, RouterModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
